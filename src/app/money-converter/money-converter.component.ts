8097717f19b1fe0eca3d9bc38ee9c375b52147f6
Jurfest <jurfest.apps@gmail.com>
2020-04-07 09:27:31 -0300

    criação estrutura da calculadora

--- src/app/money-converter/money-converter.component.ts
+++ src/app/money-converter/money-converter.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./money-converter.component.css']
 })
 export class MoneyConverterComponent implements OnInit {
-
+  numResult:number;
+  valDolar = 5.24;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  
+
+  calcOne(event){
+    let result = event.target.value;
+    this.numResult = Number(result);
+    this.numResult *= this.valDolar;
+  }
+
+  calcTwo(event){
+    let result = event.target.value;
+    this.numResult = Number(result);
+    this.numResult /= this.valDolar; 
   }
 
 }
