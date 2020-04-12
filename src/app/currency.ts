1218bc9e885e4aa5cc7166ff1400086cde98a0cd
Jurfest <jurfest.apps@gmail.com>
2020-04-12 12:08:25 -0300

    classe moeda a ser usada como modelo em money-con

---
+++ src/app/currency.ts
@@ -0,0 +1,11 @@
+// model prototype
+
+export class Currency {
+  constructor(
+    public money_name: string,
+    public rate: number,
+    public country?: string, // optional
+    public flag_img?: string, // optional
+    public id?: number // optional
+  ) { }
+}
