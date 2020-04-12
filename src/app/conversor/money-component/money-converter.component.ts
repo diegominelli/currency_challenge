768ddbc58cb70d1a3a01f6b151762ee2aac77611
Jurfest <jurfest.apps@gmail.com>
2020-04-12 16:56:19 -0300

    utilizando ngModel para os inputs da calc/conv moedas

--- src/app/conversor/money-component/money-converter.component.ts
+++ src/app/conversor/money-component/money-converter.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { ApiService } from '../money-service/api.service';
 import{Currency} from '../money-model/currency';
 import{CurrencyBase} from '../money-model/currency-base'
+import { Calculator } from '../money-model/calculator'
 
 @Component({
   selector: 'app-money-converter',
@@ -10,22 +11,15 @@ import{CurrencyBase} from '../money-model/currency-base'
   styleUrls: ['./money-converter.component.css']
 })
 export class MoneyConverterComponent implements OnInit {
+  calculator:any;
   modelMoneyBase:any;
   modelMoneyTwo:any;
-  numResult:number;
-  numResultTwo:number; // talvez não necessario
-  moneyOne:string;
-  moneyTwo:string; // talvez não necessario
-  formdata; // TODO: retirar
+
   
   constructor(private apiservice: ApiService) { 
+    this.calculator =  new Calculator(0 , 0);
     this.modelMoneyBase = new CurrencyBase("BRL");
     this.modelMoneyTwo =  new Currency("USD",20 );
-    this.moneyOne = "BRL";
-    this.moneyTwo = "USD";
-    this.numResult = 0;
-    this.numResultTwo = 0;
-    
   }
   
   entries:any;
@@ -34,37 +28,41 @@ export class MoneyConverterComponent implements OnInit {
   ngOnInit(): void {
     this.callApi();
     
+    
   }
         
   callApi() {
     let moneydata;
-    this.apiservice.getData(this.moneyOne).subscribe(
+    this.apiservice.getData(this.modelMoneyBase.money_name).subscribe(
       (data) => {
         moneydata = new Object(data);
         this.entries = Object.entries(moneydata.rates); 
         this.moneys = Object.keys(moneydata.rates);
         this.rates = Object.values(moneydata.rates);
-        console.log(`moneydata (array keys dos dados da api.rates): ${this.rates}`)
       }
-      );
-    }
+    );
+    
+    
+  }
           
   calcOne(event){
-    let result = event.target.value;
-    this.numResult = Number(result);
-    this.numResult *= this.modelMoneyTwo.rate;
+    this.calculator.inputValueTwo = this.calculator.inputValueOne / this.modelMoneyTwo.rate; 
+    // console.log(`valor de input2: ${this.calculator.inputValueTwo}`);
+    // console.log(`valor de modelMoneyTwo.rate: ${this.modelMoneyTwo.rate}`);
   }
 
   calcTwo(event){
-    let result = event.target.value;
-    this.numResultTwo = Number(result);
-    this.numResultTwo /= this.modelMoneyTwo.rate; 
+    // let result = event.target.value;
+    // this.numResultTwo = Number(result);
+    // this.numResultTwo /= this.modelMoneyTwo.rate; 
   }
 
   // TODO: submit button is disable
   onClickSubmit(data){
 
   }
+
+  // concertar abaixo:
 
   changemoneyOne(event) {
     // alterar o valor de convertionRate
@@ -88,14 +86,3 @@ export class MoneyConverterComponent implements OnInit {
 
 }
 
-
-
-// objeto retornado pela api:
-
-  // moneyobject = {"rates":{"CAD":1.528,"HKD":8.3651,"ISK":155.5,"PHP":54.667,"DKK":7.466,"HUF":365.24,"CZK":27.603,
-  // "AUD":1.779,"RON":4.8335,"SEK":10.9788,"IDR":17710.73,"INR":82.106,"BRL":5.7056,"RUB":82.481,
-  // "HRK":7.6255,"JPY":117.54,"THB":35.459,"CHF":1.056,"SGD":1.548,"PLN":4.5643,"BGN":1.9558,
-  // "TRY":7.3136,"CNY":7.6519,"NOK":11.3875,"NZD":1.82,"ZAR":20.3534,"USD":1.0791,"MXN":27.0896,
-  // "ILS":3.9144,"GBP":0.878,"KRW":1326.04,"MYR":4.7097},"base":"EUR","date":"2020-04-06"}
-
-  // moneys = Object.keys(this.moneyobject.rates);
