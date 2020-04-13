536589fa22dfae11a13182d1b8e500ea846ac12f
Jurfest <jurfest.apps@gmail.com>
2020-04-13 13:14:24 -0300

    continuando nav bar responsiva

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  //   let burger = document.querySelector('.burger');
+  //   let nav = document.querySelector('.nav-links');
+  // let navLinks = document.querySelectorAll('.nav-links li');
+  
+  toggleBurgerDisplay(event) {
+  //   // Toggle nav
+  //   event.classList.toggle("nav-active");   
+    
+  //   // Animate links
+  //   navLinks.forEach((link, index) => {
+  //     if (link.style.animation) {
+  //       link.style.animation = '';
+  //     } else {
+  //       link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+  //     }
+  //   });
+      // Burger Animation
+      // burger.classList.toggle('toggle');
+
+  }
+
+  
 
 }
